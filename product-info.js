f8ed21e57f8d8798b571fd3d14377ce00077ad76
nath-404 <nat.t.torre@gmai.com>
2025-03-27 22:53:06 +0800

    made featured carousel cards clickable, added mini database for laptop models, added product-info page (incomplete), added folder for laptop images

---
+++ product-info.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const models = JSON.parse(localStorage.getItem("selectedLaptop"));
+
+    if (models) {
+        document.getElementById("current-img").src = models.images[1];
+        document.getElementById("product-name").textContent = models.name;
+        document.getElementById("reviews").textContent = models.reviews;
+        document.getElementById("brand-name").textContent = models.brand;
+        document.getElementById("description").textContent = models.description;
+        document.getElementById("price").textContent = models.price;
+
+        // image selector
+        const imagesRepository = document.querySelectorAll(".image-repo");
+
+        imagesRepository.forEach((image, index) => {
+            image.src = models.images[index + 1];
+            console.log(models.images[index + 1]);
+        });
+
+    } else {
+        console.error("No laptop data found!");
+    }
+});
+
+
+// console.log(localStorage);
