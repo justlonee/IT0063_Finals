f081b2a114232aed074e04f244676a74b2b4f33f
nath-404 <nat.t.torre@gmai.com>
2025-03-28 13:14:25 +0800

    added spec sheet in product-info.html

--- assets/js/product-info.js
+++ assets/js/product-info.js
@@ -9,6 +9,13 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("description").textContent = models.description;
     document.getElementById("price").textContent = models.price;
 
+    document.getElementById("processor").textContent = models.specs.processor;
+    document.getElementById("display").textContent = models.specs.display;
+    document.getElementById("gpu").textContent = models.specs.gpu;
+    document.getElementById("memory").textContent = models.specs.memory;
+    document.getElementById("storage").textContent = models.specs.storage;
+    document.getElementById("os").textContent = models.specs.OS;
+
     // image selector
     const imagesRepository = document.querySelectorAll(".image-repo");
 
