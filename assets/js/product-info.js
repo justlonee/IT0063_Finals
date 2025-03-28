21b98108e6f87579d851dc189f31b26fa8c6c159
nath-404 <nat.t.torre@gmai.com>
2025-03-28 11:47:53 +0800

    fixed product-info.html images path

--- assets/js/product-info.js
+++ assets/js/product-info.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const models = JSON.parse(localStorage.getItem("selectedLaptop"));
 
   if (models) {
-    document.getElementById("current-img").src = models.images[1];
+    document.getElementById("current-img").src = "." + models.images[1];
     document.getElementById("product-name").textContent = models.name;
     document.getElementById("reviews").textContent = models.reviews;
     document.getElementById("brand-name").textContent = models.brand;
@@ -13,8 +13,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const imagesRepository = document.querySelectorAll(".image-repo");
 
     imagesRepository.forEach((image, index) => {
-      image.src = models.images[index + 1];
-      console.log(models.images[index + 1]);
+      image.src = "." + models.images[index + 1];
+      // console.log(".", models.images[index + 1]);
     });
   } else {
     console.error("No laptop data found!");
