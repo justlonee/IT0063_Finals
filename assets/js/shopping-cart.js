b04436722b3441c482ca0f542d865e810b298099
Nathan0721 <nathan.alisangco@gmail.com>
2025-03-29 20:24:50 +0800

    Added shopping cart html and css (no js yet though).

---
+++ assets/js/shopping-cart.js
@@ -0,0 +1,15 @@
+/* NOTES */
+// THERE IS NO SET MAX QUANTITY YET
+// ALL TALLY COUNTERS ARE SEPARATE PER ITEM CONTAINER
+
+// INCOMPLETE
+
+/*
+let addItem;
+let subtractItem;
+let removeProduct;
+
+let addItemButton = $(".add-button");
+let subtractItemButton = $(".subtract-button");
+let removeProductButton = $(".remove-product");
+*/
