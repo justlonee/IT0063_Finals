96b325ff89558b41475673c3c7de97477fd57ec0
Nathan0721 <nathan.alisangco@gmail.com>
2025-03-29 23:04:38 +0800

    Added properly working js to shopping cart (please check).

--- assets/js/shopping-cart.js
+++ assets/js/shopping-cart.js
@@ -2,14 +2,58 @@
 // THERE IS NO SET MAX QUANTITY YET
 // ALL TALLY COUNTERS ARE SEPARATE PER ITEM CONTAINER
 
-// INCOMPLETE
-
-/*
 let addItem;
 let subtractItem;
 let removeProduct;
 
-let addItemButton = $(".add-button");
-let subtractItemButton = $(".subtract-button");
-let removeProductButton = $(".remove-product");
-*/
+const addItemButton = $(".add-button");
+const subtractItemButton = $(".subtract-button");
+const removeProductButton = $(".remove-product");
+
+$(document).ready(function() {
+
+    function updateCart() {
+        const productItem = $(".placeholder-item");
+        let productItemCount = $(".placeholder-item").length;
+        let totalCost = 0;
+
+        productItem.each(function() {
+            let quantity = parseInt($(this).find(".item-quantity").text()) || 1;
+            let itemBasePriceText = $(this).find(".product-cost span").text().replace(/[^\d.]/g, "");
+            let itemBasePrice = parseFloat(itemBasePriceText);
+            let itemTotalPrice = itemBasePrice * quantity;
+            totalCost += itemTotalPrice;
+
+            $(this).find(".product-total-value").text(`₱${itemTotalPrice.toFixed(2)}`);
+        });
+    
+        $("#page-title span").text(`(${productItemCount} Items)`)
+        $(".total-cost").text(`₱${totalCost.toFixed(2)}`);
+    }
+
+    addItem = addItemButton.on("click", function() {
+        let $count = $(this).closest(".quantity-selector-tally").find(".item-quantity");
+        let addedCount = parseInt($count.text()) + 1;
+        $count.text(addedCount);
+        updateCart();
+    });
+
+    subtractItem = subtractItemButton.on("click", function() {
+        let $count = $(this).closest(".quantity-selector-tally").find(".item-quantity");
+        let currentCount = parseInt($count.text());
+        if (currentCount > 1) {
+            $count.text(currentCount - 1);
+            updateCart();
+        }
+    });
+
+    removeProduct = removeProductButton.on("click", function() {
+        let productItem = $(this).closest(".placeholder-item");
+        productItem.next(".product-divider").remove();
+        productItem.remove();
+        updateCart();
+    });
+
+    updateCart();
+
+});
